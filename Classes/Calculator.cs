e132a6435ec017dd89269d335582041b974ab7a2
HirsuiteLlama276 <60354361+HirsuiteLlama276@users.noreply.github.com>
2022-08-27 20:39:12 +0200

    Add files via upload
    Added the program

---
+++ Classes/Calculator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Kalkulator
+{
+    ///<summary> Szablon klasy - trzeba w programie wpisać typ </summary>
+    class Calculator<T>
+    {
+        ///<summary>  Dodawanie zmiennych </summary>
+        public  void Add(T first, T second)
+        {
+            dynamic firstnumber = first;
+            dynamic secondtnumber = second;
+            T result = firstnumber + secondtnumber;
+            Console.WriteLine(result);
+        }
+        /// <summary>Odejmowanie od pierwszej zmiennej drugiej </summary>
+        public  void Substract(T first, T second)
+        {
+            dynamic firstnumber = first;
+            dynamic secondtnumber = second;
+            T result = firstnumber - secondtnumber;
+            Console.WriteLine(result);
+        }
+        /// <summary> Mnożenie zmiennych </summary>
+        public  void Multiply(T first, T second)
+        {
+            dynamic firstnumber = first;
+            dynamic secondtnumber = second;
+            T result = firstnumber * secondtnumber;
+            Console.WriteLine(result);
+        }
+        /// <summary> Dzielenie pierwszej zmiennej przez drugą  </summary>
+        public  void Divide(T first, T second)
+        {
+            dynamic firstnumber = first;
+            dynamic secondtnumber = second;
+            T result = firstnumber / secondtnumber;
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stworzenie obiektu w którym będziemy wykonywać działania
+            var dzialania = new Calculator<float>();
+            // Przywitanie się aplikacji
+            Console.WriteLine("Witaj w Kalkulatorze!");
+            // Zapisanie zmiennych z którymi będziemy działać
+            Console.WriteLine("Podaj pierwszą liczbę ");
+            var first = float.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj drugą liczbę ");
+            var second = float.Parse(Console.ReadLine());
+            // Wypisanie możliwych działań i wczytanie działania wybranego
+            Console.WriteLine("Możliwe działania: + - * / ");
+            var operation = Console.ReadLine();
+
+            // Wykonanie wskazanego działania na obiekcie
+            switch (operation)
+            {
+                case "+":
+                    {
+                        dzialania.Add(first, second);
+                    }
+                    break;
+                case "-":
+                    {
+                         dzialania.Substract(first, second);
+                    }
+                    break;
+                case "*":
+                    {
+                         dzialania.Multiply(first, second);
+                    }
+                    break;
+                case "/":
+                    {
+                        dzialania.Divide(first, second);
+                    }
+                    break;
+
+
+            }
+            
+        }
+    }
+}
